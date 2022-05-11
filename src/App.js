263386078541b1530cad3c83c81d94605a739873
David <dvdavefenix014@gmail.com>
2022-05-11 07:58:01 -0500

    Ruta_Maya

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import tren from "./img/tren-maya (1).png" 
 import Vistas from './Components/Vistas';
 import Inicio from "./Paginas/Inicio"
+import { Fragment } from 'react';
 
 function App() {
-  return(<Inicio/>
+  return(
+    <Fragment>
+    <Router>
+      <Routes>
+        <Route path='/' element ={<Inicio/>}/>
+        <Route path='/about' element ={<div>about</div>}/>
+      </Routes>
+    </Router>  
+  
+  
+    </Fragment>
+  
   );
   
 }
